77dcee104af287a401f3192c17312aa55d571781
Parth <parthpatil5922@gmail.com>
2022-07-13 09:31:58 +0530

    Crated an arrow function to parse date from string

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const months = [
 ];
 let result;
 
+const from = to = (date) => Date.parse(date);
+
 const season = function () {
   const hemi = document.getElementById("hemisphere").value;
   const inputDate = document.getElementById("date").value;
@@ -21,6 +23,7 @@ const season = function () {
   findSeason(hemi, inputDate);
 };
 
+// Validates entries, calls calculate function and shows results.
 const findSeason = function (hemi, inputDate) {
   const [month, date] = inputDate.split(", ");
   const objDate = new Date(
@@ -33,61 +36,34 @@ const findSeason = function (hemi, inputDate) {
     !isNaN(objDate.getDate())
   ) {
     calculate(hemi, objDate);
-    document.getElementById("hemidate").innerHTML = `${(hemi === "N") ? "North" : "South"}, 
-    ${months[objDate.getMonth()][0].toUpperCase()}${months[objDate.getMonth()].slice(1)} 
+    document.getElementById("hemidate").innerHTML = `${
+      hemi === "N" ? "North" : "South"
+    }, 
+    ${months[objDate.getMonth()][0].toUpperCase()}${months[
+      objDate.getMonth()
+    ].slice(1)} 
     ${objDate.getDate()}`;
     document.getElementById("result").innerHTML = result;
-  } else document.getElementById("result").innerHTML = "Invalid Date Entered";
+  } else document.getElementById("result").innerHTML = "Invalid Data Entered";
 };
 
+// Determines Season based on hemisphere and date
 const calculate = function (hemi, objDate) {
-  if (
-    hemi === "N" &&
-    objDate >= new Date(Date.parse("03-01")) &&
-    objDate <= new Date(Date.parse("05-31"))
-  )
+  if (hemi === "N" && objDate >= from("03-01") && objDate <= to("05-31"))
     result = "Spring";
-  else if (
-    hemi === "S" &&
-    objDate >= new Date(Date.parse("03-01")) &&
-    objDate <= new Date(Date.parse("05-31"))
-  )
+  else if (hemi === "S" && objDate >= from("03-01") && objDate <= to("05-31"))
     result = "Autumn";
-  else if (
-    hemi === "N" &&
-    objDate >= new Date(Date.parse("06-01")) &&
-    objDate <= new Date(Date.parse("08-31"))
-  )
+  else if (hemi === "N" && objDate >= from("06-01") && objDate <= to("08-31"))
     result = "Summer";
-  else if (
-    hemi === "S" &&
-    objDate >= new Date(Date.parse("06-01")) &&
-    objDate <= new Date(Date.parse("08-31"))
-  )
+  else if (hemi === "S" && objDate >= from("06-01") && objDate <= to("08-31"))
     result = "Winter";
-  else if (
-    hemi === "N" &&
-    objDate >= new Date(Date.parse("09-01")) &&
-    objDate <= new Date(Date.parse("11-30"))
-  )
+  else if (hemi === "N" && objDate >= from("09-01") && objDate <= to("11-30"))
     result = "Autumn";
-  else if (
-    hemi === "S" &&
-    objDate >= new Date(Date.parse("09-01")) &&
-    objDate <= new Date(Date.parse("11-30"))
-  )
+  else if (hemi === "S" && objDate >= from("09-01") && objDate <= to("11-30"))
     result = "Spring";
-  else if (
-    hemi === "N" &&
-    (objDate >= new Date(Date.parse("12-01")) ||
-      objDate <= new Date(Date.parse("02-28")))
-  )
+  else if (hemi === "N" && (objDate >= from("12-01") || objDate <= to("02-28")))
     result = "Winter";
-  else if (
-    hemi === "S" &&
-    (objDate >= new Date(Date.parse("03-01")) ||
-      objDate <= new Date(Date.parse("05-31")))
-  )
+  else if (hemi === "S" && (objDate >= from("03-01") || objDate <= to("05-31")))
     result = "Summer";
   else console.log("Invalid");
 };
